677b9ef848053fc7eb9ec80ea29343c759f82c8b
Yina Zhu <yinagit@gmail.com>
2018-06-23 19:36:22 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -22,32 +22,20 @@ class FiguresController < ApplicationController
 
     post '/figures' do
       @figure = Figure.create(params["figure"])
-      if !params[:landmark][:name].empty?
-        @figure.landmarks << Landmark.create(params[:landmark])
-      end
 
       if !params[:title][:name].empty?
         @figure.titles << Title.create(params[:title])
       end
 
-      @figure.save
-      redirect to "/figures/#{@figure.id}"
-    end
-
-    post '/figures/:id' do
-      @figure = Figure.find(params[:id])
-      @figure.update(params[:figure])
-
       if !params[:landmark][:name].empty?
         @figure.landmarks << Landmark.create(params[:landmark])
       end
 
-      if !params[:title][:name].empty?
-        @figure.titles << Title.create(params[:title])
-      end
-
       @figure.save
+      binding.pry
       redirect to "/figures/#{@figure.id}"
     end
+
+
 
 end
